ad89dbfdeb5886e9205b8e11969cf692ad67061b
apettit53957 <andrew.pettit@nghs.com>
2026-06-10 17:34:37 -0400

    Update streamlit_app.py
    Replace FRUIT_NAME with SEARCH_ON in the API calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,9 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the Snowpark Dataframe (df) to a Pandas (pd) Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
 
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your Smoothie will be ', name_on_order)
@@ -38,8 +38,12 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen + ' Nurtrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)  
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
